a718044117a7bc167b48d37621f656af1d39d9fa
yenbekbay <ayan.yenb@gmail.com>
2021-12-18 14:44:34 +0600

    Add RenJS setup and sample game assets

--- app/routes/interactive.tsx
+++ app/routes/interactive.tsx
@@ -1,6 +1,6 @@
-import {Box, Container, Flex, Section, Separator} from '@modulz/design-system'
+import {Box, Container, Section} from '@modulz/design-system'
+import React from 'react'
 import {MetaFunction} from 'remix'
-import {Header} from '~/components/Header'
 
 // https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
@@ -11,6 +11,48 @@ export let meta: MetaFunction = () => {
 }
 
 export default function Interactive() {
+  React.useLayoutEffect(() => {
+    ;(async () => {
+      const RenJS = (await import(
+        // @ts-expect-error
+        'renjs'
+      )) as typeof import('renjs/dist/types/src')
+      const Phaser = (await import('phaser-ce')).default
+
+      const RenJSConfig: import('renjs/dist/types/src/core/RJSGameConfig').RJSGameConfig =
+        {
+          name: 'Quickstart',
+          w: 800,
+          h: 600,
+          renderer: Phaser.AUTO, // become renderer
+          scaleMode: Phaser.ScaleManager.SHOW_ALL,
+          loadingScreen: {
+            background: 'assets/game/gui/loaderloaderbackground.png',
+            loadingBar: {
+              asset: 'assets/game/gui/loaderloading-bar.png',
+              position: {
+                x: 109,
+                y: 458,
+              },
+              size: {
+                w: 578,
+                h: 82,
+              },
+            },
+          },
+          fonts: 'assets/game/gui/fonts.css',
+          guiConfig: 'story/GUI.yaml',
+          storyConfig: 'story/Config.yaml',
+          storySetup: 'story/Setup.yaml',
+          storyText: ['story/Story.yaml'],
+          logChoices: true,
+        }
+
+      const RenJSGame = new RenJS.game(RenJSConfig)
+      RenJSGame.launch()
+    })()
+  }, [])
+
   return (
     <Box>
       <Section size="2">
