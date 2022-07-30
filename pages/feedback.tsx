3e3c50988b9ee9c31b16d44d1f2ff124a9e10991
yenbekbay <ayan.yenb@gmail.com>
2022-07-30 20:00:45 +0600

    Fix bug on feedback page

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,36 +1,32 @@
 import {bgArchcodeOfficeJpg} from 'assets/game'
 import {Hero, HeroBackground, Layout, RoughCard} from 'components'
 import {DiscussionEmbed} from 'disqus-react'
-import {useSavedLinks} from 'game/LinkPrompt'
 
-export default function Links() {
-  const savedLinks = useSavedLinks()
+export default function Feedback() {
   return (
     <Layout>
       <main>
         <HeroBackground src={bgArchcodeOfficeJpg} className="bg-cover" />
 
         <Hero title="Отзывы">
-          <p>Здесь вы можете оставить отзыв, пожелание или предложение</p>
+          <p>Здесь вы можете оставить отзыв, пожелание или предложение.</p>
         </Hero>
 
-        {savedLinks.length > 0 && (
-          <section className="container mx-auto">
-            <div className="mx-auto flex max-w-full flex-col py-8 md:w-[640px] md:px-8 lg:mx-0">
-              <RoughCard contentClassName="max-w-none">
-                <DiscussionEmbed
-                  shortname="archcode-heritage-novel"
-                  config={{
-                    url: 'https://heritage-novel.com',
-                    identifier: 'default',
-                    title: 'Визуальная новелла «Снести нельзя оставить»',
-                    language: 'ru',
-                  }}
-                />
-              </RoughCard>
-            </div>
-          </section>
-        )}
+        <section className="container mx-auto">
+          <div className="mx-auto flex max-w-full flex-col py-8 md:w-[640px] md:px-8 lg:mx-0">
+            <RoughCard contentClassName="max-w-none">
+              <DiscussionEmbed
+                shortname="archcode-heritage-novel"
+                config={{
+                  url: 'https://heritage-novel.com',
+                  identifier: 'default',
+                  title: 'Визуальная новелла «Снести нельзя оставить»',
+                  language: 'ru',
+                }}
+              />
+            </RoughCard>
+          </div>
+        </section>
       </main>
     </Layout>
   )
